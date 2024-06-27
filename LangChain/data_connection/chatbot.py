bcc1943169fb00b2aefc38c7bf07a33b76d133a6
Geaun <kge2015@gmail.com>
2024-06-27 23:23:36 +0900

    PDF에서 학습한 내용으로 답변 생성
    데이터 저장소(벡터 DB)로 Chroma 사용

---
+++ LangChain/data_connection/chatbot.py
@@ -0,0 +1,10 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain.indexes.vectorstore import VectorstoreIndexCreator
+
+loader = PyPDFLoader("./data/서울특별시 스마트도시 및 정보화 기본계획(홈페이지 게시용).pdf")
+
+index = VectorstoreIndexCreator(embedding=OpenAIEmbeddings()).from_loaders([loader])
+print("질문을 입력하세요")
+answer = index.query(input())
+print(answer)
