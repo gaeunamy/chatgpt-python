8a56d838b417e6ce48a982e2874db2e73f0fb97b
Geaun <kge2015@gmail.com>
2024-06-27 22:59:34 +0900

    오디오 파일 읽어 전사

---
+++ Whisper/text.py
@@ -0,0 +1,10 @@
+from openai import OpenAI
+client = OpenAI()
+
+file = open("sample.wav", "rb")
+transcript = client.audio.transcriptions.create (
+    model="whisper-1",
+    file=file,
+)
+
+print(transcript.text)
